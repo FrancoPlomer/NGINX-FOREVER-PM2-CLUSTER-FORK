56508782410a267993f0f672d73575d59f0f615c
PlomerFranco <franko.plomer@gmail.com>
2022-04-30 05:07:28 -0300

    ultimos cambios

---
+++ guia-comandos.sh
@@ -0,0 +1,20 @@
+#Modo cluster nativo:
+node server.js --modo cluster --puerto 8080
+
+#Modo cluster:
+node server.js --modo cluster
+
+#Modo fork
+npm start
+
+#Procesos forever:
+forever list
+
+#Listado de procesos:
+tasklist
+
+#Listado de procesos pm2:
+pm2 list
+
+#matar todos los procesos:
+npm stop
